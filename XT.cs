99ea6372e2e09b1d200535723cc994ee635d48de
folkert van heusden <mail@vanheusden.com>
2023-04-27 01:44:08 +0200

    1st commit

---
+++ XT.cs
@@ -0,0 +1,7 @@
+namespace XT
+{
+    class p8086
+    {
+	    int[] memory = new int[1024 * 1024];  // 1MB of RAM
+    }
+}
---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+using XT;
+
+namespace dotxt
+{
+    class DotXT
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DotXT, (C) 2023 by Folkert van Heusden");
+            Console.WriteLine("Released under in the public domain");
+
+	    p8086 p = new p8086();
+        }
+    }
+}
