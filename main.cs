e925f046cefa559a9e637322a02838bd71542f23
folkert van heusden <mail@vanheusden.com>
2023-04-27 10:29:38 +0200

    tabs

--- main.cs
+++ main.cs
@@ -10,7 +10,7 @@ namespace dotxt
             Console.WriteLine("DotXT, (C) 2023 by Folkert van Heusden");
             Console.WriteLine("Released under in the public domain");
 
-	    p8086 p = new p8086();
+            p8086 p = new p8086();
         }
     }
 }
