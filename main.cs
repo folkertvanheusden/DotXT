fdc7240ae13a009510de1748ddb08934310b02eb
folkert van heusden <mail@vanheusden.com>
2023-04-27 14:06:57 +0200

    "typo"

--- main.cs
+++ main.cs
@@ -8,7 +8,7 @@ namespace dotxt
         static void Main(string[] args)
         {
             Console.WriteLine("DotXT, (C) 2023 by Folkert van Heusden");
-            Console.WriteLine("Released under in the public domain");
+            Console.WriteLine("Released in the public domain");
 
             p8086 p = new p8086();
 
