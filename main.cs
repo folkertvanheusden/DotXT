b2c60117bcd5ef7210a7539a44a827e6e3cb9d0c
Joris Zwart <hallo@joriszwart.nl>
2023-04-28 11:07:59 +0200

    use top-level statements
    Note that you can still use `args` etc.

--- main.cs
+++ main.cs
@@ -1,19 +1,9 @@
-using System;
 using XT;
 
-namespace dotxt
-{
-    class DotXT
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("DotXT, (C) 2023 by Folkert van Heusden");
-            Console.WriteLine("Released in the public domain");
+Console.WriteLine("DotXT, (C) 2023 by Folkert van Heusden");
+Console.WriteLine("Released in the public domain");
 
-            p8086 p = new p8086();
+p8086 p = new p8086();
 
-            for(;;)
-                p.tick();
-        }
-    }
-}
+for (;;)
+    p.tick();
