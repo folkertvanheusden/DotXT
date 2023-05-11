7832b1f76b1e7bf059632d9d469a9638d5547353
folkert van heusden <mail@vanheusden.com>
2023-05-11 15:00:56 +0200

    combine cpu_test and regular mode in one

--- main.cs
+++ main.cs
@@ -3,7 +3,18 @@ using DotXT;
 Console.WriteLine("DotXT, (C) 2023 by Folkert van Heusden");
 Console.WriteLine("Released in the public domain");
 
-var p = new P8086();
+bool do_test = false;
+
+foreach(var s in args)
+{
+    if (s == "test")
+    {
+        do_test = true;
+        break;
+    }
+}
+
+var p = new P8086(do_test);
 
 for (;;)
     p.Tick();
