folkertvanheusden/DotXT

commit cd27074992fb6644f70186da484ce3eda25075cb

IN AX,DX fix (0xec)

XT.cs
@@ -3164,7 +3164,7 @@ internal class P8086
 
             int mreg = o1 & 7;
 
-            // get address to write to ('seg, addr'))
+            // get address to write to ('seg, addr')
             (ushort dummy, string name, bool a_valid, ushort seg, ushort addr) = GetRegisterMem(mreg, mod, word);
 
             if (word)
@@ -3544,11 +3544,11 @@ internal class P8086
             // LOOPNZ
             byte to = GetPcByte();
 
-            (ushort cx, string dummy) = GetRegister(1, true);
+            ushort cx = GetCX();
 
             cx--;
 
-            PutRegister(1, true, cx);
+            PutCX(cx);
 
             ushort newAddresses = (ushort)(_ip + (sbyte)to);
 
@@ -3564,11 +3564,11 @@ internal class P8086
             // LOOP
             byte to = GetPcByte();
 
-            (ushort cx, string dummy) = GetRegister(1, true);
+            ushort cx = GetCX();
 
             cx--;
 
-            PutRegister(1, true, cx);
+            PutCX(cx);
 
             ushort newAddresses = (ushort)(_ip + (sbyte)to);
 
@@ -3603,8 +3603,8 @@ internal class P8086
         }
         else if (opcode == 0xec)
         {
-            // IN AL,DX
-            _al = _io.In(_scheduled_interrupts, GetDX());
+            // IN AX,DX
+            SetAX(_io.In(_scheduled_interrupts, GetDX()));
 
 #if DEBUG
             Log.DoLog($"{prefixStr} IN AL,DX");

commit 90cee8f17e9700d4a1bccdbdc2352e1ac655b84d

cd27074992fb6644f70186da484ce3eda25075cb broke 0xec (IN AL,DX)

XT.cs
@@ -3739,15 +3739,6 @@ internal class P8086
             Log.DoLog($"{prefixStr} IN AX,${from:X2}");
 #endif
         }
-        else if (opcode == 0xec)
-        {
-            // IN AX,DX
-            SetAX(_io.In(_scheduled_interrupts, GetDX()));
-
-#if DEBUG
-            Log.DoLog($"{prefixStr} IN AL,DX");
-#endif
-        }
         else if (opcode == 0xe6)
         {
             // OUT
@@ -3757,6 +3748,15 @@ internal class P8086
 
 #if DEBUG
             Log.DoLog($"{prefixStr} OUT ${to:X2},AL");
+#endif
+        }
+        else if (opcode == 0xec)
+        {
+            // IN AL,DX
+            _al = _io.In(_scheduled_interrupts, GetDX());
+
+#if DEBUG
+            Log.DoLog($"{prefixStr} IN AL,DX");
 #endif
         }
         else if (opcode == 0xee)