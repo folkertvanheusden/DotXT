4995f1f1c6852954ea059f558912cd941aa821c1
folkert van heusden <mail@vanheusden.com>
2023-07-05 17:34:45 +0200

    comment

--- XT.cs
+++ XT.cs
@@ -1647,7 +1647,7 @@ internal class P8086
         }
         else if (opcode == 0xa7)
         {
-            // CMPSB
+            // CMPSW
             ushort v1 = ReadMemWord(_segment_override_set ? _segment_override : _ds, _si);
             ushort v2 = ReadMemWord(_es, _di);
 
