9d68ade4db4770547ff4360bbacd5efc251ea261
folkert van heusden <mail@vanheusden.com>
2023-07-07 08:42:53 +0200

    sahf/lahf test

--- tests/misc3.py
+++ tests/misc3.py
@@ -36,6 +36,7 @@ test_001_dest:
     dw 0
 
 test_001_do:
+    mov si,#$0001
     mov ax,#test_001_source
     add ax,#8
     mov si,ax
@@ -85,6 +86,7 @@ test_002_dest:
     dw 0
 
 test_002_do:
+    mov si,#$0002
     mov ax,#test_002_source
     mov si,ax
 
@@ -119,6 +121,7 @@ test_002_xfer_ok:
 test_003:
     dw $7766
 test_003_go:
+    mov si,#$0003
     cld
     mov ax,#test_003
     mov si,ax
@@ -134,6 +137,7 @@ test_003_ok1:
 test_003_ok2:
 
 test_004:
+    mov si,#$0004
     mov ax,#$9911
     not ax
     cmp ax,#$66ee
@@ -145,6 +149,7 @@ test_004_ok:
 test_005:
     dw $1199
 test_005_go:
+    mov si,#$0005
     not [test_005]
     mov ax,[test_005]
     cmp ax,#$ee66
@@ -153,7 +158,9 @@ test_005_go:
 test_005_ok:
 
 test_006:
-    mov ax,#$ffff
+    mov si,#$0006
+    ; make sure not to enable interrupts
+    mov ax,#$fdff
     push ax
     popf
     lahf
@@ -161,7 +168,7 @@ test_006:
     beq test_006_ok1
     hlt
 test_006_ok1:
-    cmp al,#$00
+    cmp al,#$ff
     beq test_006_ok2
     hlt
 test_006_ok2:
@@ -177,6 +184,20 @@ test_006_ok3:
     beq test_006_ok4
     hlt
 test_006_ok4:
+
+test_007:
+    mov si,#$0007
+    mov ax,#$fdff
+    push ax
+    popf
+    mov al,#$00
+    sahf
+    pushf
+    pop ax
+    cmp al,#$d7
+    beq test_007_ok1
+    hlt
+test_007_ok1:
 
 finish:
 ''')
