430216216539da90b816aca35223c60b44055d1d
folkert van heusden <mail@vanheusden.com>
2023-07-09 15:09:39 +0200

    half of the CGA memory is attributes

--- CGA.cs
+++ CGA.cs
@@ -92,12 +92,16 @@ class CGA : Device
                 uint y = use_offset / (80 * 2);
                 uint x = (use_offset % (80 * 2)) / 2;
 
-                Log.DoLog($"CGA::WriteByte {x},{y} = {(char)value}");
+                // attribute, character
+                if ((x & 1) == 1)
+                {
+                    Log.DoLog($"CGA::WriteByte {x},{y} = {(char)value}");
 
-                Console.Write((char)27);  // position cursor
-                Console.Write($"[{y + 1};{x + 1}H");
+                    Console.Write((char)27);  // position cursor
+                    Console.Write($"[{y + 1};{x + 1}H");
 
-                Console.Write((char)value);
+                    Console.Write((char)value);
+                }
             }
         }
     }
