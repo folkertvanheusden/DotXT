5635f82f2ae95a09c11b6cf2f0c62ba2c5ea22ba
folkert van heusden <mail@vanheusden.com>
2023-07-17 17:35:56 +0200

    oob error fix

--- IO.cs
+++ IO.cs
@@ -351,7 +351,7 @@ internal class pic8259
 
         Log.DoLog($"8259 IN: read cache for addr {addr:X4}");
 
-        return (_register_cache[addr], false);
+        return (_register_cache[addr - 0x0020], false);
     }
 
     public void Tick()
@@ -364,7 +364,7 @@ internal class pic8259
 
         Log.DoLog($"8259 OUT port {addr} value {value:X2}");
 
-        _register_cache[addr] = value;
+        _register_cache[addr - 0x0020] = value;
 
         if (addr == 0x0020)
         {
