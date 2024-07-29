144f6c45e58f9ea7b136359a414c3997c5b6e92a
folkert van heusden <mail@vanheusden.com>
2024-07-29 09:02:22 +0200

    compile when run

--- interrupt.sh
+++ interrupt.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
+as86 -0 -b interrupt.bin -l interrupt.list interrupt.asm
 rm ~/temp/ramdisk/logfile.txt ; dotnet run -- -t interrupt.bin -T 0 -o 0,0800 -x binary -l ~/temp/ramdisk/logfile.txt
