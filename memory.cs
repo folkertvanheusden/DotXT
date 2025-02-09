024470ae6b5a907c9b388c07f6d37c63c0b50d1f
Folkert van Heusden <mail@vanheusden.com>
2025-02-09 20:45:03 +0100

    working (+/-) settings

--- memory.cs
+++ memory.cs
@@ -39,7 +39,6 @@ internal class Rom
         _contents = File.ReadAllBytes(filename);
 
         string annotations_file = filename + ".ann";
-
         if (File.Exists(annotations_file))
         {
             Console.WriteLine($"Loading annotations file {annotations_file}");
@@ -125,18 +124,11 @@ class Bus
 {
     private Memory _m;
 
-//    private readonly Rom _bios = new("roms/BIOS_5160_16AUG82_U18_5000026.BIN");
-    private readonly Rom _bios = new("roms/glabios/GLaBIOS/src/GLABIOS.ROM");
+//    private readonly Rom _bios = new("roms/10jan86-bios/BIOS_5160_10JAN86_U18_62X0851_27256_F800.BIN");
+    private readonly Rom _bios = new("roms/GLABIOS.ROM");
+//    private readonly Rom _bios = new("roms/xtramtest.32k");
 
-    // private readonly Rom _bios = new("roms/ruuds_diagnostic_rom_32kb_2022-12-12.bin");
-    // private readonly Rom _bios = new("roms/DiagROM/DiagROM");
-    // private readonly Rom _bios = new("roms/diagrom201212/DIAGROMV2.bin");
-
-    // private readonly Rom _bios = new("roms/Supersoft_PCXT_8KB.bin");
-    private readonly Rom _basic = new("roms/BIOS_5160_16AUG82_U19_5000027.BIN");
-    // private readonly Rom _bios = new("roms/GLABIOS_0.2.4_8T.ROM");
-    // private readonly Rom _bios = new("roms/GLABIOS_0.2.4_8P.ROM");
-    // private readonly Rom _bios = new("roms/BIOS_5160_09MAY86_U18_59X7268_62X0890_27256_F800.BIN");
+    private readonly Rom _basic = new("roms/10jan86-bios/BIOS_5160_10JAN86_U19_62X0854_27256_F000.BIN");
     // private readonly Rom _basic = new("roms/BIOS_5160_09MAY86_U19_62X0819_68X4370_27256_F000.BIN");
 
     private List<Device> _devices;
