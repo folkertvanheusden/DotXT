0c567d68008fe48852bbde4f114cdf41965c274d
Folkert van Heusden <mail@vanheusden.com>
2025-02-20 23:03:17 +0100

    basic keyboard

--- keyboard.cs
+++ keyboard.cs
@@ -4,7 +4,8 @@ class Keyboard : Device
 {
     private Thread _keyboard_thread;
     protected int _irq_nr = 1;
-    protected int _irq_trigger_delay = 4770;  // cycles for 1ms @ 4.77 MHz
+    protected int _kb_reset_irq_delay = 4770;  // cycles for 1ms @ 4.77 MHz
+    protected int _kb_key_irq = 100;
     private Mutex _keyboard_buffer_lock = new();
     private Queue<int> _keyboard_buffer = new();
     private bool _clock_low = false;
@@ -12,6 +13,7 @@ class Keyboard : Device
 
     public Keyboard()
     {
+        Console.TreatControlCAsInput = true;
         _keyboard_thread = new Thread(Keyboard.KeyboardThread);
         _keyboard_thread.Name = "keyboard_thread";
         _keyboard_thread.Start(this);
@@ -30,7 +32,7 @@ class Keyboard : Device
         _keyboard_buffer.Enqueue(scan_code);
         _keyboard_buffer_lock.ReleaseMutex();
 
-        ScheduleInterrupt(_irq_trigger_delay);  // the value is a guess, need to protect this with a mutex
+        ScheduleInterrupt(_kb_key_irq);
     }
 
     public static void KeyboardThread(object o_kb)
@@ -39,17 +41,69 @@ class Keyboard : Device
 
         Keyboard kb = (Keyboard)o_kb;
 
+        Dictionary<ConsoleKey, byte> key_map = new() {
+            { ConsoleKey.Escape, 0x01 },
+            { ConsoleKey.Enter, 0x1c },
+            { ConsoleKey.D1, 0x02 },
+            { ConsoleKey.D2, 0x03 },
+            { ConsoleKey.D3, 0x04 },
+            { ConsoleKey.D4, 0x05 },
+            { ConsoleKey.D5, 0x06 },
+            { ConsoleKey.D6, 0x07 },
+            { ConsoleKey.D7, 0x08 },
+            { ConsoleKey.D8, 0x09 },
+            { ConsoleKey.D9, 0x0a },
+            { ConsoleKey.D0, 0x0b },
+            { ConsoleKey.A, 0x1e },
+            { ConsoleKey.B, 0x30 },
+            { ConsoleKey.C, 0x2e },
+            { ConsoleKey.D, 0x20 },
+            { ConsoleKey.E, 0x12 },
+            { ConsoleKey.F, 0x21 },
+            { ConsoleKey.G, 0x22 },
+            { ConsoleKey.H, 0x23 },
+            { ConsoleKey.I, 0x17 },
+            { ConsoleKey.J, 0x24 },
+            { ConsoleKey.K, 0x25 },
+            { ConsoleKey.L, 0x26 },
+            { ConsoleKey.M, 0x32 },
+            { ConsoleKey.N, 0x31 },
+            { ConsoleKey.O, 0x18 },
+            { ConsoleKey.P, 0x19 },
+            { ConsoleKey.Q, 0x10 },
+            { ConsoleKey.R, 0x13 },
+            { ConsoleKey.S, 0x1f },
+            { ConsoleKey.T, 0x14 },
+            { ConsoleKey.U, 0x16 },
+            { ConsoleKey.V, 0x2f },
+            { ConsoleKey.W, 0x11 },
+            { ConsoleKey.X, 0x2d },
+            { ConsoleKey.Y, 0x15 },
+            { ConsoleKey.Z, 0x2c },
+            { ConsoleKey.Spacebar,  0x39 },
+            { ConsoleKey.Backspace, 0x0e },
+        };
+
         for(;;)
         {
             ConsoleKeyInfo cki = Console.ReadKey(true);
             ConsoleKey ck = cki.Key;
 
-            Log.DoLog($"Key pressed: {cki.Key.ToString()}", true);
+            Log.DoLog($"Key pressed: {cki}", true);
 
-            if (ck == ConsoleKey.F1)  // F1
-                kb.PushKeyboardScancode(0x3a);
-	    else
-                kb.PushKeyboardScancode(cki.KeyChar);
+            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
+                kb.PushKeyboardScancode(0x2a);  // shift make
+            if ((cki.Modifiers & ConsoleModifiers.Control) != 0)
+                kb.PushKeyboardScancode(0x1d);  // control make
+            if (key_map.ContainsKey(ck))
+            {
+                kb.PushKeyboardScancode(key_map[ck]);  // key make
+                kb.PushKeyboardScancode(key_map[ck] ^ 0x80);  // unmake
+            }
+            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0)
+                kb.PushKeyboardScancode(0x2a ^ 0x80);  // shift unmake
+            if ((cki.Modifiers & ConsoleModifiers.Control) != 0)
+                kb.PushKeyboardScancode(0x1d ^ 0x80);  // control unmake
         }
 
         Log.DoLog("KeyboardThread terminating", true);
@@ -86,7 +140,7 @@ class Keyboard : Device
                 _keyboard_buffer.Enqueue(0xaa);  // power on reset reply
                 _keyboard_buffer_lock.ReleaseMutex();
 
-                ScheduleInterrupt(_irq_trigger_delay);  // the value is a guess, need to protect this with a mutex
+                ScheduleInterrupt(_kb_reset_irq_delay);  // the value is a guess, need to protect this with a mutex
             }
         }
 
@@ -101,11 +155,15 @@ class Keyboard : Device
             byte rc = 0;
             if (_keyboard_buffer.Count > 0)
                 rc = (byte)_keyboard_buffer.Dequeue();
+            bool interrupt_needed = _keyboard_buffer.Count > 0;
             _keyboard_buffer_lock.ReleaseMutex();
 
-            Log.DoLog($"Keyboard: scan code {rc:X2}", true);
+            Log.DoLog($"Keyboard: scan code {rc}", true);
 
-            return (rc, false);
+            if (interrupt_needed)
+                ScheduleInterrupt(_kb_key_irq);
+
+            return (rc, interrupt_needed);
         }
         else if (port == 0x61)
             return (_0x61_bits, false);
@@ -117,7 +175,10 @@ class Keyboard : Device
             bool keys_pending = _keyboard_buffer.Count > 0;
             _keyboard_buffer_lock.ReleaseMutex();
 
-            return ((byte)(keys_pending ? 0x21 : 0x20), false);  // TODO 0x21/0x20?
+            if (keys_pending)
+                ScheduleInterrupt(_kb_key_irq);
+
+            return ((byte)((keys_pending ? 2 : 0) | 0x10), keys_pending);
         }
 
         return (0x00, false);
