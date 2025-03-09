17995f4f1f46b3b95adbd053b16493c45d24807e
Folkert van Heusden <mail@vanheusden.com>
2025-03-09 11:41:07 +0100

    XT-server emulation enhancements

--- XTServer.cs
+++ XTServer.cs
@@ -1,18 +1,22 @@
 using DotXT;
 
+using System.Text;
+
 internal class XTServer : Device
 {
     private int _irq_nr = -1;
     private string _bin_file;
     private uint _bin_file_load_address = 0;
     private Display _d = null;
+    private string _trace_file = null;
 
-    public XTServer(string bin_file, uint bin_file_load_address, Display d)
+    public XTServer(string bin_file, uint bin_file_load_address, Display d, string trace_file)
     {
         Log.Cnsl("XTServer instantiated");
         _bin_file = bin_file;
         _bin_file_load_address = bin_file_load_address;
         _d = d;
+        _trace_file = trace_file;
     }
 
     public override int GetIRQNumber()
@@ -28,6 +32,9 @@ internal class XTServer : Device
     public override void RegisterDevice(Dictionary <ushort, Device> mappings)
     {
         mappings[0xf001] = this;
+        mappings[0xf063] = this;
+        mappings[0xf064] = this;
+        mappings[0xf065] = this;
     }
 
     private void LoadBin(Bus b, string file, uint addr)
@@ -91,6 +98,30 @@ internal class XTServer : Device
             else if (value == 0xff)  //  load test code
             {
                 LoadBin(_b, _bin_file, _bin_file_load_address);
+            }
+        }
+        else if (port == 0xf063)
+        {
+            if (_trace_file != null)
+            {
+                using (FileStream stream = new FileStream(_trace_file, FileMode.Append, FileAccess.Write))
+                {
+                    byte[] data = new UTF8Encoding(true).GetBytes($"{value:X4}");
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+        else if (port == 0xf064 || port == 0xf065)
+        {
+            // f065 has special escape codes TODO
+
+            if (_trace_file != null)
+            {
+                using (FileStream stream = new FileStream(_trace_file, FileMode.Append, FileAccess.Write))
+                {
+                    byte[] data = new byte[] { (byte)value };
+                    stream.Write(data, 0, data.Length);
+                }
             }
         }
 
--- xtserver_optionrom.asm
+++ xtserver_optionrom.asm
@@ -9,8 +9,31 @@
 	out dx,al
 
 ; set interrupts
+	xor ax,ax
+	mov ds,ax
+
+	mov si,#($60 * 4)
+	mov cx,#$0b
+set_loop:
+	mov [si + 0],#do_nothing
+	mov [si + 2],#$d000
+	add si, #$04
+	loopnz set_loop
+
 	mov si,#($60 * 4)
 	mov [si + 0],#screenshot
+	mov [si + 2],#$d000
+
+	mov si,#($63 * 4)
+	mov [si + 0],#send_ax
+	mov [si + 2],#$d000
+
+	mov si,#($64 * 4)
+	mov [si + 0],#process_64
+	mov [si + 2],#$d000
+
+	mov si,#($65 * 4)
+	mov [si + 0],#process_65
 	mov [si + 2],#$d000
 
 ; start program
@@ -21,12 +44,41 @@ ax_storage:
 dx_storage:
 	dw 0
 
+do_nothing:
+	iret
+
 screenshot:
 	mov [ax_storage], ax
 	mov [dx_storage], dx
 	mov dx,#$f001
-	mov al,#$01
-	out dx,al
+	mov ax,#$6001
+	out dx,ax
 	mov dx,[dx_storage]
 	mov ax,[ax_storage]
+	iret
+
+send_ax:
+	mov [dx_storage], dx
+	mov dx,#$f063
+	out dx,ax
+	mov dx,[dx_storage]
+	iret
+
+process_64:
+	mov [dx_storage], dx
+	mov dx,#$f064
+send_64:
+	mov al,[si]
+	inc si
+	out dx,al
+	dec cx
+	loopnz send_64
+	mov dx,[dx_storage]
+	iret
+
+process_65:
+	mov [dx_storage], dx
+	mov dx,#$f065
+	out dx,al
+	mov dx,[dx_storage]
 	iret
