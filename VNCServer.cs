564ad09e67b9c161548b8eaa3852df16b6799ed7
Folkert van Heusden <mail@vanheusden.com>
2025-08-05 23:08:09 +0200

    ,

--- VNCServer.cs
+++ VNCServer.cs
@@ -134,6 +134,7 @@ class VNCServer: GraphicalConsole
                 { 0x79, new byte[] { 0x15 } },
                 { 0x7a, new byte[] { 0x2c } },  // z
                 { 0x20, new byte[] { 0x39 } },  // space
+                { 0x2c, new byte[] { 0x33 } },  // ,
                 { 0x2e, new byte[] { 0x34 } },  // .
                 { 0x25, new byte[] { 0x06 } },  // %
                 { 0x24, new byte[] { 0x05 } },  // $
