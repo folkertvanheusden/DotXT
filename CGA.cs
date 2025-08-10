c3134b420b58bdb4970d032f3892dd889a445d26
Folkert van Heusden <mail@vanheusden.com>
2025-08-10 19:48:03 +0200

    double line fix

--- CGA.cs
+++ CGA.cs
@@ -408,7 +408,7 @@ class CGA : Display
                 _gf.rgb_pixels[offset + 0] = _gf.rgb_pixels[offset + 3] = rgb[0];
                 _gf.rgb_pixels[offset + 1] = _gf.rgb_pixels[offset + 4] = rgb[1];
                 _gf.rgb_pixels[offset + 2] = _gf.rgb_pixels[offset + 5] = rgb[2];
-                offset += 320 * 3 * 4;
+                offset += 320 * 3 * 2;
                 _gf.rgb_pixels[offset + 0] = _gf.rgb_pixels[offset + 3] = rgb[0];
                 _gf.rgb_pixels[offset + 1] = _gf.rgb_pixels[offset + 4] = rgb[1];
                 _gf.rgb_pixels[offset + 2] = _gf.rgb_pixels[offset + 5] = rgb[2];
